dee1cf9c5e1654b4ae0a4ddf9c92739f2b378bdc
StrayDragon <straydragonl@foxmail.com>
2021-03-31 11:30:40 +0800

    prepare: 测试用例和util

---
+++ models/README.md
@@ -0,0 +1,10 @@
+# 命名规则
+
+- \<model>.\<case>.json: 待测试原始请求json
+- \<model>.schema.json: 用于某些校验库的schema
+
+## \<case>
+
+- ok: 正常通过
+- err1: 有一处校验错误
+- errall: 全部校验错误 
---
+++ utils.go
@@ -0,0 +1,43 @@
+package benchmark_validate_go
+
+import (
+	"io/ioutil"
+	"path"
+	"path/filepath"
+	"runtime"
+)
+
+var (
+	_, b, _, _     = runtime.Caller(0)
+	basePath       = filepath.Dir(b)
+	modelsJSONPath = path.Join(basePath, "models")
+)
+
+func BasePath() string {
+	return basePath
+}
+
+func ReadJSONFile(name string) []byte {
+	targetPath := GetTargetJSONFilePath(name)
+	bytesValue, err := ioutil.ReadFile(targetPath)
+	if err != nil {
+		panic(err.Error())
+	}
+	return bytesValue
+}
+
+func GetTargetJSONFilePath(name string) string {
+	if filepath.Ext(name) != ".json" {
+		panic("Cannot read non-json file!")
+	}
+	targetPath := path.Join(modelsJSONPath, name)
+	return targetPath
+}
+
+func GetTargetJSONFileStandardPath(name string) string {
+	if filepath.Ext(name) != ".json" {
+		panic("Cannot read non-json file!")
+	}
+	targetPath := path.Join(modelsJSONPath, name)
+	return "file://" + targetPath
+}
